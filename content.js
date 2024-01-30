f13382a00520381719e231da49f2a46c1a3704be
LKPcode <spectatingtheweb@gmail.com>
2024-01-30 03:23:07 +0200

    Chat colors appear selected in the menu

--- content.js
+++ content.js
@@ -1,211 +1,199 @@
-
-const MENU_PARENT_SELECTOR = "main div.sticky";
+// Constants for DOM selectors and color classes
+const MENU_PARENT_SELECTOR = "main";
 const EXT_MENU_SELECTOR = 'div[ext-menu]';
-
+const CHATS_LIST_SELECTOR = "nav div.flex-col div.flex-col";
+const CHAT_LINK_SELECTOR = "nav ol li a[href^='/c/']";
 const colorClasses = {
     1: 'bg-red-500',
     2: 'bg-green-500',
-    3: 'bg-blue-500',
+    3: 'bg-yellow-500',
     none: ''
 };
 
-// {
-//     "id": string,
-//     "color": string,
-// }
+// Global variables
 let chats = [];
 let menu = null;
 
 
-async function setColorMenu() {
-
-
-    // Programatically create menu based on colorClasses
-    let menuHtml = `<div class='w-full justify-center mt-4 right-2 top-[50px] flex gap-4 items-center' ext-menu>`;
-    for (let colorClass in colorClasses) {
-        if (colorClass == 'none') {
-            menuHtml += `<div class='w-4 h-4 bg-white rounded-full hover:cursor-pointer' ext-color='${colorClass}'></div>`;
-            continue;
-        }
-        menuHtml += `<div class='w-4 h-4 ${colorClasses[colorClass]} rounded-full hover:cursor-pointer' ext-color='${colorClass}'></div>`;
-    }
-    menuHtml += `</div>`;
-
-
-
-    let chatsList = document.querySelector("nav div.flex-col div.flex-col");
-    console.log("Chats list: " + chatsList);
-    let parent = chatsList.parentElement;
-    console.log("Parent: " + parent);
-   
-
-    
-
-    let extMenuDiv = parent.querySelector('div[ext-menu]');
-    if (!extMenuDiv) {
-        extMenuDiv = document.createElement('div');
-        // add attribute ext-menu
-        extMenuDiv.setAttribute('ext-menu', '');
-        // set css style
-        extMenuDiv.setAttribute('style', 'top: 50px;padding-bottom: 20px;')
-        // set class string
-        extMenuDiv.setAttribute('class', 'sticky left-0 right-0 z-20 bg-black pt-3.5');
-
-        extMenuDiv.innerHTML = menuHtml;
-
-        parent.insertBefore(extMenuDiv, chatsList);
-        menu = extMenuDiv;
-        console.log("New div with ext-menu added");
-
-        // add event listeners to menu items
-        menu.querySelectorAll('div[ext-color]').forEach(menuItem => {
-            menuItem.addEventListener('click', (event) => {
-                let clicked_color = event.target.getAttribute('ext-color')
-                let chat_id = window.location.href.split('/')[4];
-
-                console.log("Clicked color: " + clicked_color);
-                console.log("Chat id: " + chat_id);
-                
-                // Change color of chat
-                setColorToChat(chat_id, clicked_color);
-
-            });
-        });
-    }
+// Initialization function for the extension
+function initExtension() {
+    initChatLocalStorage();
+    watchForChatListChanges();
+    watchForChatChanges();
+    handleInitialSelectedChat();
 }
 
-function setColorToChat(chatId, color) {
-    let chat_element = document.querySelector("nav ol li a[href='/c/" + chatId + "']")
-
-    if (!chat_element) {
-        console.log("Chat element not found");
-        updateLocalStorage({
-            "id": chatId,
-            "color": color
-        });
-        return;
-    }
-
-    //  check if div with attribute ext-chat exists as child to the parent of the chat element
-    let color_element = chat_element.parentElement.querySelector("div[ext-chat]");
-    if (color_element) {
-        // remove color element
-        color_element.remove();
-    }
-
-    let color_html = `<div ext-chat='${chatId}' class="absolute ext-chatColor top-1/2 -translate-y-1/2 w-1 h-1/3 rounded-full ${colorClasses[color]} " style='height: 33%;'></div>`;
-    // Add color html as sibling to chat element
-    chat_element.insertAdjacentHTML('afterend', color_html);
-
-    // Update local storage
-    updateLocalStorage({
-        "id": chatId,
-        "color": color
-    });
-   
-}
-
-function initChatLocalStorage () {
-    // If key extention-chats does not exist, create it
-    if (!localStorage.getItem('extention-chats')) {
-        localStorage.setItem('extention-chats', JSON.stringify([]));
+// Local Storage Management
+function initChatLocalStorage() {
+    if (!localStorage.getItem('extension-chats')) {
+        localStorage.setItem('extension-chats', JSON.stringify([]));
     }
 }
 
 function getChatsFromLocalStorage() {
-    return JSON.parse(localStorage.getItem('extention-chats'));
+    return JSON.parse(localStorage.getItem('extension-chats'));
 }
 
 function updateLocalStorage(chat) {
-    let chats_ = getChatsFromLocalStorage();
-    let chat_color = chat.color;
-    // if chat color is none, remove chat from local storage
-    if (chat_color == 'none') {
-        chats_ = chats_.filter(function(value, index, arr){ return value.id != chat.id;});
+    let chats = getChatsFromLocalStorage();
+    let chatIndex = chats.findIndex(c => c.id === chat.id);
+    if (chat.color === 'none') {
+        if (chatIndex !== -1) chats.splice(chatIndex, 1);
     } else {
-        //else if chat already exists, update color, if not, add chat to local storage
-        let chat_index = chats_.findIndex(function(value, index, arr){ return value.id == chat.id;});
-        if (chat_index != -1) {
-            chats_[chat_index].color = chat_color;
+        if (chatIndex !== -1) {
+            chats[chatIndex].color = chat.color;
         } else {
-            chats_.push(chat);
+            chats.push(chat);
         }
     }
-    
-
-    localStorage.setItem('extention-chats', JSON.stringify(chats_));
+    localStorage.setItem('extension-chats', JSON.stringify(chats));
 }
 
+// Chat List and Color Menu Management
+function setupColorMenu() {
+    if (!document.querySelector(EXT_MENU_SELECTOR)) {
+        createColorMenu();
+        addMenuEventListeners();
+    }
+}
 
+function createColorMenu() {
+    let menuHtml = `<div class='w-full justify-center mt-4 right-2 top-[50px] flex gap-4 items-center' ext-menu>`;
+    for (let color in colorClasses) {
+        menuHtml += `<div class='w-4 h-4 ${colorClasses[color]} rounded-full hover:cursor-pointer' ext-color='${color}'></div>`;
+    }
+    menuHtml += `</div>`;
 
+    let chatList = document.querySelector(CHATS_LIST_SELECTOR);
+    let extMenuDiv = document.createElement('div');
+    extMenuDiv.innerHTML = menuHtml;
+    extMenuDiv.setAttribute('ext-menu', '');
+    extMenuDiv.setAttribute('style', 'top: 50px;padding-bottom: 20px;');
+    extMenuDiv.setAttribute('class', 'sticky left-0 right-0 z-20 bg-black pt-3.5');
+    chatList.parentElement.insertBefore(extMenuDiv, chatList);
+    menu = extMenuDiv;
+}
 
-initChatLocalStorage();
+function addMenuEventListeners() {
+    if (menu) {
+        menu.querySelectorAll('div[ext-color]').forEach(menuItem => {
+            menuItem.addEventListener('click', event => handleColorSelection(event));
+        });
+    }
+}
 
-// setTimeout(() => {
-//     setColorMenu();
+// Chat Color Selection and Update
+function handleColorSelection(event) {
+    let clickedColor = event.target.getAttribute('ext-color');
+    let chatId = getSelectedChatId();
+    if (chatId) {
+        let currentColor = getCurrentColorForChat(chatId);
+        let newColor = currentColor === clickedColor ? 'none' : clickedColor;
+        setColorToChat(chatId, newColor);
+        updateSelectedColorInMenu(newColor);
+    }
+}
 
-// }, 3000); 
+function getCurrentColorForChat(chatId) {
+    let chats = getChatsFromLocalStorage();
+    let chat = chats.find(chat => chat.id === chatId);
+    return chat ? chat.color : 'none';
+}
 
+function setColorToChat(chatId, color) {
+    let chatElement = document.querySelector(`nav ol li a[href='/c/${chatId}']`);
+    if (!chatElement) {
+        updateLocalStorage({"id": chatId, "color": color});
+        return;
+    }
+    let colorElement = chatElement.parentElement.querySelector(`div[ext-chat='${chatId}']`);
+    if (colorElement) {
+        colorElement.remove();
+    }
+    if (color !== 'none') {
+        let colorHtml = `<div ext-chat='${chatId}' class="absolute ext-chatColor top-1/2 -translate-y-1/2 w-1 h-1/3 rounded-full ${colorClasses[color]} " style='height: 33%;'></div>`;
+        chatElement.insertAdjacentHTML('afterend', colorHtml);
+    }
+    updateLocalStorage({"id": chatId, "color": color});
+}
 
+function updateSelectedColorInMenu(selectedColor) {
+    console.log("updateSelectedColorInMenu", selectedColor, menu);
+    if (menu) {
+        menu.querySelectorAll('div[ext-color]').forEach(div => {
+            div.style.border = div.getAttribute('ext-color') === selectedColor ? '2px solid white' : 'none';
+        });
+    }
+}
 
-
-
-// Usage example
-const disconnectChatObserver = setupObserver("nav div.flex-col div.flex-col", () => {
-    console.log("Mutation in Chat observed");
-    let chats_ = getChatsFromLocalStorage();
-    chats_.forEach(chat => {
-        setColorToChat(chat.id, chat.color);
+// DOM Observers
+function watchForChatListChanges() {
+    setupObserver(CHATS_LIST_SELECTOR, () => {
+        let chats = getChatsFromLocalStorage();
+        chats.forEach(chat => {
+            setColorToChat(chat.id, chat.color);
+        });
+        setupColorMenu();
+        handleInitialSelectedChat();
     });
-    setColorMenu();
+}
 
-});
-
-// const disconnectBarObserver = setupObserver("main", () => {
-//     console.log("Mutation in Bar observed");
-//     setColorMenu();
-// });
-
+function watchForChatChanges() {
+    setupObserver(MENU_PARENT_SELECTOR, () => {
+        let chatId = getSelectedChatId();
+        console.log("watchForChatChanges -> Chat changed to ChatID", chatId);
+        if (chatId) {
+            let chats = getChatsFromLocalStorage();
+            let foundChat = chats.find(chat => chat.id === chatId);
+            if (foundChat) {
+                updateSelectedColorInMenu(foundChat.color);
+            } else {
+                updateSelectedColorInMenu('none');
+            }
+        }
+    });
+}
 
 function setupObserver(targetSelector, callback, debounceDelay = 400) {
-    // Select the target element
     const targetElement = document.querySelector(targetSelector);
     if (!targetElement) {
-      console.warn(`Element not found: ${targetSelector}`);
-      return;
+        console.warn(`Element not found: ${targetSelector}`);
+        return;
     }
-  
-    // Debounce function to limit the rate of invocation of the callback
+
     let debounceTimer;
     const debouncedCallback = () => {
-      clearTimeout(debounceTimer);
-      debounceTimer = setTimeout(callback, debounceDelay);
+        clearTimeout(debounceTimer);
+        debounceTimer = setTimeout(callback, debounceDelay);
     };
-  
-    // Create an observer instance linked to the debounced callback function
-    const observer = new MutationObserver((mutations) => {
-      debouncedCallback();
+
+    const observer = new MutationObserver(() => {
+        debouncedCallback();
     });
-  
-    // Configuration of the observer
-    const config = {
-      childList: true,  // Observe direct children
-      subtree: false,   // Observe all descendants
-      attributes: true  // Observe attributes changes
-    };
-  
-    // Start observing the target element for configured mutations
+
+    const config = { childList: true, subtree: false, attributes: true };
     observer.observe(targetElement, config);
-  
-    // Function to disconnect the observer
-    const disconnectObserver = () => observer.disconnect();
-  
-    // Return the disconnect function in case it needs to be called externally
-    return disconnectObserver;
-  }
-  
-  
-  // To disconnect the observer when it's no longer needed
-  // disconnectChatObserver();
-  // disconnectBarObserver();
-  
+
+    return () => observer.disconnect();
+}
+
+// Initial Chat Selection Handling
+function handleInitialSelectedChat() {
+    const selectedChatId = getSelectedChatId();
+    console.log("SelectedChatID", selectedChatId);
+    if (selectedChatId) {
+        const chats = getChatsFromLocalStorage();
+        const foundChat = chats.find(chat => chat.id === selectedChatId);
+        console.log("FoundChat", foundChat);
+        if (foundChat) {
+            updateSelectedColorInMenu(foundChat.color);
+        }
+    }
+}
+
+function getSelectedChatId() {
+    return window.location.href.split('/')[4];
+}
+
+// Initialize the extension
+initExtension();
